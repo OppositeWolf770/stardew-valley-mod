79972964bb6403e2535bf5db69df376bfbf49ae3
OppositeWolf770 <shschool42@gmail.com>
2023-06-02 18:35:49 -0500

    Merge Block Test

--- src/main/java/net/daniel/stardewvalley/block/custom/MergeFile.java
+++ src/main/java/net/daniel/stardewvalley/block/custom/MergeFile.java
@@ -18,7 +18,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 
-// TODO - Finish adding the hitboxes for the scarecrow directions
+// TODO - Finish adding the hitboxes for Thank you for killing yourself
 public class MergeFile extends Block {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     protected static final VoxelShape EAST_SHAPE = Shapes.or(
