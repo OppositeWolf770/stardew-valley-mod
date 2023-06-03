ecf501e16895b1cbb8ec3b3c913e4a39455493bf
OppositeWolf770 <shschool42@gmail.com>
2023-06-03 09:15:39 -0500

    Changed files according to new File Structure.

--- src/main/java/net/daniel/stardewvalley/item/ModCreativeModeTab.java
+++ src/main/java/net/daniel/stardewvalley/item/ModCreativeModeTab.java
@@ -7,28 +7,28 @@ public class ModCreativeModeTab {
     public static final CreativeModeTab STARDEW_TAB = new CreativeModeTab("stardewtab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.ARTIFACT_CHICKEN_STATUE.get());
+            return new ItemStack(ModArtifacts.ARTIFACT_CHICKEN_STATUE.get());
         }
     };
 
     public static final CreativeModeTab ARTIFACTS_TAB = new CreativeModeTab("artifactstab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.ARTIFACT_LOST_BOOK.get());
+            return new ItemStack(ModArtifacts.ARTIFACT_LOST_BOOK.get());
         }
     };
 
     public static final CreativeModeTab MINERALS_TAB = new CreativeModeTab("mineralstab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.QUARTZ.get());
+            return new ItemStack(ModMinerals.QUARTZ.get());
         }
     };
 
     public static final CreativeModeTab WEAPONS_TAB = new CreativeModeTab("weaponstab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.GALAXY_SWORD.get());
+            return new ItemStack(ModWeapons.GALAXY_SWORD.get());
         }
     };
 }
